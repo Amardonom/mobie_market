19b493792329acc1b95bd77a9114cc4d2e798679
work <15375459@qq.com>
2019-01-28 17:50:49 +0800

    注册页面

--- service/index.js
+++ service/index.js
@@ -7,7 +7,7 @@ const {connect , initSchemas} = require('./database/init.js')
     await connect()
     initSchemas()
     const User = mongoose.model('User')
-    let oneUser = new User({userName:'jspang13',password:'123456'})
+    let oneUser = new User({userName:'jspang',password:'123456'})
     oneUser.save().then(()=>{
         console.log('插入成功')
     })
